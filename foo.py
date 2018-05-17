286077a04696d57262a367fb7140901ac0f688e5
Yaw-Ling Lin <yawlin@gmail.com>
2018-05-17 12:15:36 +0800

    addin fun def fibo

--- foo.py
+++ foo.py
@@ -11,3 +11,13 @@ print("it is 5 lines")
 
 # This is a comment -- 05/15/18
 print("Here output the test text; this is coded from Visual Sudio Code -- 05/15/18.")
+
+# Building a python recursive function
+def fibo(n):
+    if (n <= 1) :
+        return(n)
+    else:
+        return ( fibo(n-1) + fibo(n-2) )
+
+for i in range(10):
+    print( 'fibo(%d) = %d' % ( i, fibo(i) ) )
